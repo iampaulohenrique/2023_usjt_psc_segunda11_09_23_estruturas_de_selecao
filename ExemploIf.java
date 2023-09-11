823a9c86d6c2fd8955890162b34e17c9e30cad1d
Paulo Henrique <phlpaulohenriqueluiz@gmail.com>
2023-09-11 20:53:25 -0300

    ap03(selecao): import e estrutura

---
+++ ExemploIf.java
@@ -0,0 +1,14 @@
+//java.lang é o pacote padrao do java
+import javax.swing.JOptionPane;
+public class ExemploIf{
+    static public void main (String [] args){
+        
+        //declaracao de variáveis
+
+        //Entrada
+        
+        //processamento
+        
+        //saida
+    }
+}
